b396476be2438298a836d537a8091005aea31884
pkthunder87 <pkthunder87@gmail.com>
2023-12-13 01:51:54 -0800

    Feat: Add mobile version to Step.jsx

--- src/pages/welcome/Step.jsx
+++ src/pages/welcome/Step.jsx
@@ -13,20 +13,21 @@ function Step({
   return (
     <section className=" col-span-2 grid grid-cols-2" aria-label={ariaLabel}>
       <div
-        className={` relative bg-cream  text-[5rem] leading-[1.2em] ${textColor}`}
+        className={` relative bg-cream text-[1.7rem]  leading-[1.3em] sm:text-[5rem] ${textColor}`}
       >
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform ">
           <p>
-            Step <span className="text-[6rem]">{stepNum}</span>
+            Step <span className="text-3xl sm:text-[6rem]">{stepNum}</span>
           </p>
           <p>{stepText}</p>
         </div>
       </div>
-      <img
-        className={` h-[50dvw] w-[50dvw] ${bgColor} ${imgZPattern}`}
-        src={imgDesk}
-        alt={imgAlt}
-      />
+
+      <picture className={` h-[50dvw] w-[50dvw] ${bgColor} ${imgZPattern}`}>
+        <source media="(max-width:441px)" srcSet={imgMobi} type="image/webp" />
+        <source media="(min-width:442px)" srcSet={imgDesk} type="image/webp" />
+        <img src={imgOut} alt={imgAlt} type="image/jpg" />
+      </picture>
     </section>
   );
 }
--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -21,6 +21,8 @@ function Welcome() {
           ariaLabel="step-1"
           imgAlt="Pastel doodle of women camping under a starry night"
           imgDesk="./travel-desktop.webp"
+          imgMobi="./travel-mobile.webp"
+          imgOut="./travel-outdated.jpg"
           stepNum={1}
           stepText="Have an Adventure"
           textColor="text-pale-blue"
@@ -31,6 +33,8 @@ function Welcome() {
           ariaLabel="step-2"
           imgAlt="Pastel doodle of phone with GPS marker and women sitting nearby"
           imgDesk="./gps-desktop.webp"
+          imgMobi="./gps-mobile.webp"
+          imgOut="./gps-outdated.jpg"
           stepNum={2}
           imgZPattern="-order-1"
           stepText="Mark the Location"
@@ -42,6 +46,8 @@ function Welcome() {
           ariaLabel="step-3"
           imgAlt="Pastel doodle of man with pencil in front of browser window"
           imgDesk="./entry-desktop.webp"
+          imgMobi="./entry-mobile.webp"
+          imgOut="./entry-outdated.jpg"
           stepNum={3}
           stepText="Create an Entry"
           textColor="text-periwinkle"
