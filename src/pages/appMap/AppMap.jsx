7ee2094ca2e4b97dae000f630abb5b1f88d6376b
pkthunder87 <pkthunder87@gmail.com>
2023-12-21 15:57:34 -0800

    Feat: Add interactive leaflet map

--- src/pages/appMap/AppMap.jsx
+++ src/pages/appMap/AppMap.jsx
@@ -2,6 +2,8 @@ import { NavLink, useNavigate } from 'react-router-dom';
 
 import { useAuth } from '../../contexts/FakeAuthContext';
 import Header from '../../ui/Header';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useState } from 'react';
 
 function AppMap() {
   const { logout } = useAuth();
@@ -11,42 +13,32 @@ function AppMap() {
     logout();
     navigate('/');
   }
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
 
   // useEffect(function () {
   //   getCabins().then((data) => console.log(data));
   // }, []);
 
   return (
-    <div>
-      <Header
-        noLogout={false}
-        linkToSolid="/welcome"
-        solidText="Home"
-        outlineText="Logout"
-      />
-      <div className="flex flex-col gap-4">
-        <button
-          onClick={handleClick}
-          className="button-general w-32 bg-periwinkle"
+    <div className="h-[100dvh] w-[100-dvw] sm:grid sm:grid-cols-[35%_65%] md:grid-cols-[27%_73%]">
+      <div className="bg-tint-teal">Left Side</div>
+      <div className="h-full w-full bg-white">
+        <MapContainer
+          center={mapPosition}
+          zoom={13}
+          scrollWheelZoom={true}
+          className="h-full w-full"
         >
-          Logout
-        </button>
-
-        <NavLink to="/app/entries">
-          <button className="button-general w-32 bg-periwinkle">
-            AppEntries
-          </button>
-        </NavLink>
-
-        <NavLink to="/app/filter">
-          <button className="button-general w-32 bg-periwinkle">
-            AppFilter
-          </button>
-        </NavLink>
-
-        <NavLink to="/app/form">
-          <button className="button-general w-32 bg-periwinkle">AppForm</button>
-        </NavLink>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={mapPosition}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );
