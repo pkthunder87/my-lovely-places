70690a6fb5ae83a208e3963c7386eb03c3e0f9f4
pkthunder87 <pkthunder87@gmail.com>
2024-01-15 17:18:42 -0800

    Refactor: Remove unnecessary code from supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import { useCurrentUser } from '../contexts/UserContext';
 
 const supabaseUrl = 'https://euonjwwdtbyhkwleqcek.supabase.co';
-// const supabaseKey = process.env.SUPABASE_KEY;
+
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
